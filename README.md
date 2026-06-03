4a13039af91c2042f8f8e8344c279676e8aa2df1
Gabriel Morais Marcondes <gmorais671@gmail.com>
2026-06-03 11:41:21 -0300

    Commit 001: Estrutura de pastas e stubs

---
+++ README.md
@@ -0,0 +1,4 @@
+# uniqswims_landing
+
+Landing page para o projeto UniqSwims.
+Desenvolvida em Flutter Web.
---
+++ android/app/src/main/kotlin/com/example/uniqswims_lp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.uniqswims_lp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
